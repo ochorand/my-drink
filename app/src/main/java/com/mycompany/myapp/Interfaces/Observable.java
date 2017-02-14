8222933820252bc8a2759bfcf16a366667c4787f
ochorand <chorand88@hotmail.com>
2017-02-14 22:01:20 +0100

    maj 2017 02 14

---
+++ app/src/main/java/com/mycompany/myapp/Interfaces/Observable.java
@@ -0,0 +1,7 @@
+package com.mycompany.myapp.Interfaces;
+
+public interface Observable
+{
+	
+	
+}
---
+++ app/src/main/java/com/mycompany/myapp/Interfaces/Observateur.java
@@ -0,0 +1,6 @@
+package com.mycompany.myapp.Interfaces;
+
+public interface Observateur
+{
+	public void update();
+}
